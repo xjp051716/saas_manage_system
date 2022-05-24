7d048805399ccce2968be3186e25ac757abcbd01
夏剑平 <xjp0517@163.com>
2022-05-24 15:38:19 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -40,6 +41,12 @@ export default defineConfig({
     Icons({
       autoInstall: true,
     }),
+    viteCompression({
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: true
+    })
   ],
   resolve: {
     alias: {
