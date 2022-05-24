7611f8d03fa061c135e797aa08c09a3b57b2be0a
夏剑平 <xjp0517@163.com>
2022-05-24 16:03:59 +0800

    第三方插件打包配置

--- vite.config.js
+++ vite.config.js
@@ -53,7 +53,13 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
+      external: ['vue','element-plus','echarts'],
       output: {
+        globals: {
+          vue: 'Vue',
+          'element-plus': 'elementPlus',
+          echarts: 'echarts'
+        },
         manualChunks(id) {
           // 初始化tailwindcss文件，放入至main.js中路径一致
           if (id.includes("assets/css/index.css")) {
